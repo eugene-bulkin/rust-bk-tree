fb91fe8319a4a01f95149a56c6bf2f3ba654b304
Eugene Bulkin <ebulkin@caltech.edu>
2016-02-13 23:06:04 -0800

    Fix tests to check that the vectors are same unordered

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,19 @@
 extern crate bk_tree;
 
+use std::fmt::Debug;
+
 use bk_tree::{BKNode, BKTree};
 use bk_tree::metrics::levenshtein;
+
+fn assert_eq_sorted<T>(left: Vec<T>, right: Vec<T>) where T: Ord + Clone + Debug {
+    let mut left_mut = left.to_vec();
+    let mut right_mut = right.to_vec();
+
+    left_mut.sort();
+    right_mut.sort();
+
+    assert_eq!(left_mut, right_mut);
+}
 
 #[test]
 fn node_construct() {
@@ -46,9 +58,9 @@ fn tree_find() {
     tree.add("boon");
     tree.add("cook");
     tree.add("cart");
-    assert_eq!(tree.find("caqe", 1), vec!["cake", "cape"]);
-    assert_eq!(tree.find("cape", 1), vec!["cake", "cape"]);
-    assert_eq!(tree.find("book", 1), vec!["book", "books", "boo", "boon", "cook"]);
-    assert_eq!(tree.find("book", 0), vec!["book"]);
+    assert_eq_sorted(tree.find("caqe", 1), vec!["cake", "cape"]);
+    assert_eq_sorted(tree.find("cape", 1), vec!["cake", "cape"]);
+    assert_eq_sorted(tree.find("book", 1), vec!["book", "books", "boo", "boon", "cook"]);
+    assert_eq_sorted(tree.find("book", 0), vec!["book"]);
     assert!(tree.find("foobar", 1).is_empty());
 }
