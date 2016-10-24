6383bd3c9bd228e6c0f27d05ffa7f0cef62eb1d6
Chris Wong <lambda.fairy@gmail.com>
2016-10-24 20:28:59 +1300

    Make BKTree more idiomatic
    * The metric function is exposed as a type parameter. This allows for
    static dispatch, and parallels the `Hasher` parameter of `HashMap`.
    
    * The `.find()` and `.find_exact()` methods are changed to use the
    `Borrow` trait. This lets us search a `BKTree<String>` using a `&str`
    argument, for example.
    
    * The Levenshtein function takes `AsRef<str>` instead of `ToString`.

--- src/metrics.rs
+++ src/metrics.rs
@@ -4,6 +4,8 @@
 
 use std::cmp::min;
 
+use Metric;
+
 /// This calculates the Levenshtein distance between two strings.
 ///
 /// The [distance metric itself][1] is calculated using the [Wagner-Fischer][2]
@@ -12,63 +14,69 @@ use std::cmp::min;
 /// # Examples
 ///
 /// ```
-/// use bk_tree::metrics::levenshtein;
+/// use bk_tree::Metric;
+/// use bk_tree::metrics::Levenshtein;
 ///
-/// assert_eq!(levenshtein("bar", "baz"), 1);
-/// assert_eq!(levenshtein("kitten", "sitting"), 3);
+/// assert_eq!(Levenshtein.distance("bar", "baz"), 1);
+/// assert_eq!(Levenshtein.distance("kitten", "sitting"), 3);
 /// ```
 ///
 /// [1]: https://en.wikipedia.org/wiki/Levenshtein_distance
 /// [2]: https://en.wikipedia.org/wiki/Wagner%E2%80%93Fischer_algorithm
-pub fn levenshtein<S: ToString>(a: S, b: S) -> u64 {
-    let str_a: &str = &a.to_string();
-    let str_b: &str = &b.to_string();
+pub struct Levenshtein;
 
-    let len_a: u64 = str_a.chars().collect::<Vec<char>>().len() as u64;
-    let len_b: u64 = str_b.chars().collect::<Vec<char>>().len() as u64;
-    if len_a == 0 {
-        return len_b;
-    }
-    if len_b == 0 {
-        return len_a;
-    }
+impl<K: AsRef<str> + ?Sized> Metric<K> for Levenshtein
+{
+    fn distance(&self, a: &K, b: &K) -> u64 {
+        let str_a: &str = a.as_ref();
+        let str_b: &str = b.as_ref();
 
-    // This is a case-insensitive algorithm
-    let a_lower = str_a.to_lowercase();
-    let b_lower = str_b.to_lowercase();
+        let len_a: u64 = str_a.chars().collect::<Vec<char>>().len() as u64;
+        let len_b: u64 = str_b.chars().collect::<Vec<char>>().len() as u64;
+        if len_a == 0 {
+            return len_b;
+        }
+        if len_b == 0 {
+            return len_a;
+        }
 
-    // Initialize the array
-    let mut d: Vec<Vec<u64>> = Vec::new();
-    for j in 0..(len_b + 1) {
-        let mut cur_vec = Vec::new();
-        for i in 0..(len_a + 1) {
-            if j == 0 {
-                cur_vec.push(i);
-            } else if i == 0 {
-                cur_vec.push(j);
-            } else {
-                cur_vec.push(0);
+        // This is a case-insensitive algorithm
+        let a_lower = str_a.to_lowercase();
+        let b_lower = str_b.to_lowercase();
+
+        // Initialize the array
+        let mut d: Vec<Vec<u64>> = Vec::new();
+        for j in 0..(len_b + 1) {
+            let mut cur_vec = Vec::new();
+            for i in 0..(len_a + 1) {
+                if j == 0 {
+                    cur_vec.push(i);
+                } else if i == 0 {
+                    cur_vec.push(j);
+                } else {
+                    cur_vec.push(0);
+                }
+            }
+            d.push(cur_vec);
+        }
+
+        for j in 0..len_b as usize {
+            for i in 0..len_a as usize {
+                let chr_a = a_lower.chars().nth(i).unwrap();
+                let chr_b = b_lower.chars().nth(j).unwrap();
+                if chr_a == chr_b {
+                    // If they're the same, then don't modify the value
+                    d[j + 1][i + 1] = d[j][i];
+                } else {
+                    // Otherwise, pick the lowest cost option for an error
+                    let deletion = d[j + 1][i] + 1;
+                    let insertion = d[j][i + 1] + 1;
+                    let substitution = d[j][i] + 1;
+                    d[j + 1][i + 1] = min(min(deletion, insertion), substitution);
+                }
             }
         }
-        d.push(cur_vec);
-    }
 
-    for j in 0..len_b as usize {
-        for i in 0..len_a as usize {
-            let chr_a = a_lower.chars().nth(i).unwrap();
-            let chr_b = b_lower.chars().nth(j).unwrap();
-            if chr_a == chr_b {
-                // If they're the same, then don't modify the value
-                d[j + 1][i + 1] = d[j][i];
-            } else {
-                // Otherwise, pick the lowest cost option for an error
-                let deletion = d[j + 1][i] + 1;
-                let insertion = d[j][i + 1] + 1;
-                let substitution = d[j][i] + 1;
-                d[j + 1][i + 1] = min(min(deletion, insertion), substitution);
-            }
-        }
+        d[len_b as usize][len_a as usize]
     }
-
-    d[len_b as usize][len_a as usize]
 }
