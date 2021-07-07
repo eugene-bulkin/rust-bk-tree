7efe94acd3d1a0b300da508d1e1492f9ccbeb7c2
Mike Burr <mb@unintuitive.org>
2021-07-07 14:52:03 -0600

    pub `BKTree::root`

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@ where
 #[derive(Debug)]
 pub struct BKTree<K, M = metrics::Levenshtein> {
     /// The root node. May be empty if nothing has been put in the tree yet.
-    root: Option<BKNode<K>>,
+    pub root: Option<BKNode<K>>,
     /// The metric being used to determine the distance between nodes on the
     /// tree.
     metric: M,
